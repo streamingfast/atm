9d044676fe158446de92fb12211b69d783213945
colindickson <colin.dickson@gmail.com>
2022-05-19 08:24:54 -0400

    io: added panic recovery in delete to log error more clearly

--- io.go
+++ io.go
@@ -1,6 +1,8 @@
 package atm
 
 import (
+	"fmt"
+	"go.uber.org/zap"
 	"io/ioutil"
 	"os"
 )
@@ -25,6 +27,19 @@ func (f *FileIO) Read(path string) ([]byte, error) {
 	return ioutil.ReadFile(path)
 }
 
-func (f *FileIO) Delete(path string) error {
-	return os.Remove(path)
+func (f *FileIO) Delete(path string) (err error) {
+	defer func() {
+		if r := recover(); r != nil {
+			zlog.Error("file delete: recovered from panic. if this happens too often disk might fill to capacity.", zap.Any("error", r))
+			err = fmt.Errorf("recovered error while deleting file %s: %+v", path, r)
+		}
+	}()
+
+	err = os.Remove(path)
+	if err != nil {
+		zlog.Error("deleting file", zap.Error(err), zap.String("path", path))
+		return
+	}
+
+	return
 }
